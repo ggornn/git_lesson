e56dcf1539d03ee8208286f27a1748be152fc669
Lefleur <34391445+ggornn@users.noreply.github.com>
2018-01-17 19:45:14 +0300

    added documantation about ** operator

--- my_square.py
+++ my_square.py
@@ -1,5 +1,8 @@
 def my_square(x):
-	"""Takes a value and returns the aquares value"""
+	"""Takes a value and returns the aquares value
+	
+	uses ** operator
+	"""
 	return(x ** 2)
 
 print(my_square(3))
